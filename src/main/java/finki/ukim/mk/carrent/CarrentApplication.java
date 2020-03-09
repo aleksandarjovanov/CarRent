61d2f70e93bc6cce42d1a18960c3a546aceb6639
RabbitBox <jovanov.aleksandar997@gmail.com>
2020-03-09 19:40:10 +0100

    start point

---
+++ src/main/java/finki/ukim/mk/carrent/CarrentApplication.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.carrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrentApplication.class, args);
+    }
+
+}
